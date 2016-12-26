21025d8b87c3741bea73252f1af6c11bc3dc6065
alfredmincinoiu <alfred.mincinoiu@gmail.com>
2016-12-27 00:29:41 +0100

    reeeeepo

---
+++ src/main/java/chatserver/TCPManagerThread.java
@@ -0,0 +1,62 @@
+package chatserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Created by alfredmincinoiu on 26/12/2016.
+ */
+class TCPManagerThread implements Runnable {
+
+    private Chatserver chatserver;
+    private ServerSocket serverSocket;
+    private ArrayList<TCPIOThread> tcpioThreads;
+
+    public TCPManagerThread(Chatserver chatserver) {
+        tcpioThreads = new ArrayList<>();
+        this.chatserver = chatserver;
+        try {
+            serverSocket = new ServerSocket(chatserver.getConfig().getInt("tcp.port"));
+        }
+        catch (IOException e) {
+            chatserver.getUserResponseStream().println("Server broke down");
+        }
+
+    }
+
+    public void run() {
+        try {
+            while (serverSocket!= null && !serverSocket.isClosed()) {
+                Socket tcpSocket = serverSocket.accept();
+                TCPIOThread cliThread = new TCPIOThread(chatserver, tcpSocket);
+                chatserver.getExecutorService().execute(cliThread);
+                tcpioThreads.add(cliThread);
+            }
+        }
+        catch (IOException e) {
+            chatserver.getUserResponseStream().println("Server broke down");
+        }
+    }
+
+    public void exit() {
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+            for (TCPIOThread tt : tcpioThreads) {
+                if(tt != null) {
+                    tt.exit();
+                }
+            }
+        } catch (IOException e) {
+            chatserver.getUserResponseStream().println("Server broke down");
+        }
+    }
+
+    public ArrayList<TCPIOThread> getTcpioThreads() {
+        return tcpioThreads;
+    }
+
+}
